aa7e199233208d696cea9e4b74089e0b80880ced
Abdul Azeez Adediran <abdulazeez@Clarences-MacBook-Pro.local>
2019-08-21 12:38:38 +0800

    Aos file addded

---
+++ script/script.js
@@ -0,0 +1,27 @@
+const hamburger = document.querySelector('.menu');
+const navLink = document.querySelector('nav');
+const Links = document.querySelectorAll('ul li');
+
+hamburger.addEventListener('click', () => {
+    navLink.classList.toggle('active');
+    Links.forEach(link => {
+        link.classList.toggle('fade');
+    });
+});
+
+let i = 0;
+const text = "October 19, 2019";
+const speed = 50;
+
+function type() {
+    let alphabet = text.charAt(i);
+
+    document.getElementById("eventDate").innerHTML += alphabet;
+    i++;
+    setTimeout(type, speed);
+
+};
+
+type();
+
+AOS.init(); 
