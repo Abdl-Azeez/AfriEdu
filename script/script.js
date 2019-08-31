3844e047fce2c83c52752f879205f017602f863e
Abdl-Azeez <hazeezadediran@gmail.com>
2019-08-31 12:19:00 +0800

    About Section Initiated

--- script/script.js
+++ script/script.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.menu');
 const navLink = document.querySelector('nav');
 const Links = document.querySelectorAll('ul li');
+const navHeader = document.querySelector('.navigation-bar');
 
 hamburger.addEventListener('click', () => {
-    navLink.classList.toggle('active');
+    navLink.classList.toggle('Navtoggle');
+    // navLink.toggle('bacolor');
     Links.forEach(link => {
         link.classList.toggle('fade');
     });
@@ -22,55 +24,27 @@ function type() {
 
 
 };
-
+// hamburger.addEventListener('click',function () {
+//     $(navHeader).toggleClass('nav-scroll');
+// });
 type();
-
 AOS.init();
 
-
-// var targetDate = new Date(2020, 0, 1, 0, 0, 0);  // target date
-
-// function calcDays()
-// {
-//   var currentDate = new Date();
-//   var day  = (targetDate - currentDate) / (1000 * 60 * 60 * 24);
-
-//    return day;
-// }
-
-// function calcHours(day)
-// {
-//    var hour = (day  - Math.floor(day)) * 24;
-
-//    return hour;
-// }
-
-// function calcMin(hour)
-// {
-//    var min  = (hour  - Math.floor(hour)) * 60;
-
-//    return min;
-// }
-
-// function calcSec(min)
-// {
-//    var sec  = (min  - Math.floor(min)) * 60;
-//    return sec;
-// }
-
-//  function countdown()
-// {
-//    var day  = calcDays();
-//    var hour = calcHours(day);
-//    var min  = calcMin(hour);
-//    var sec  = calcSec(min);
-
-//    document.getElementById("fday").innerHTML  = Math.floor(day);
-//    document.getElementById("fhour").innerHTML = Math.floor(hour);
-//    document.getElementById("fmin").innerHTML  = Math.floor(min);
-//    document.getElementById("fsec").innerHTML  = Math.floor(sec);
-// } 
-
+addEventListener("load", function () {
+    $(window).scroll(function () {
+      if ($(window).scrollTop() >= 70) {
+        // $('.navigation-bar').addClass('nav-scroll');
+        $(".navigation-bar").fadeIn(500, function() {
+            $(this).addClass("nav-scroll");
+        });
+      } else {
+        // $('.navigation-bar').removeClass('nav-scroll');
+        $(".navigation-bar").fadeIn("slow", function() {
+            $(this).removeClass("nav-scroll");
+        });
+      }
+    });
+  });
 
 
 var targetDate = new Date(2019, 9, 30, 0, 0, 0); // target date
